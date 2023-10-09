17b28a8d1af6a8e9f0286a3ce08d157c01b1674b
abdur-png <118098200+abdur-png@users.noreply.github.com>
2023-10-08 20:25:08 -0400

    Add files via upload

---
+++ user/sanity.c
@@ -0,0 +1,21 @@
+#include "user.h"
+
+// Fibonacci sequence
+int fib(int n) {
+  if(n <= 1) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+
+// Use this for long computation
+// fib(39) takes roughly 30 secs
+void longComputation() {
+  fib(39);
+}
+
+
+int main(int argc, char* argv[]) {
+  // Your code can start here
+  exit();
+}
